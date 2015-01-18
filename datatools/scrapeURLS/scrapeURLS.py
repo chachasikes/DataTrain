aebfd3fdc78fa9b602df470f3a08c51b61b28e0e
Chacha Sikes <chachasikes@gmail.com>
2015-01-17 17:41:46 -0800

    reworking way that csv file is generated (using object)

--- datatools/scrapeURLS/scrapeURLS.py
+++ datatools/scrapeURLS/scrapeURLS.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import sys, os, urllib2, json, urllib, requests
+import sys, os, urllib2, json, urllib, requests,datetime
 from bs4 import BeautifulSoup
 
 # Read file and return cropped file, or random sample.
@@ -9,19 +9,48 @@ def process_file(file_path, limit, selector, process_only):
     lines = f.readlines()
     line_count = file_len(file_path)
     print "Total number urls: " + str(line_count)
+    
 
     count = 0
     text = ''
+    csv_object_array = []
     for line in lines:
         if count < int(limit):
-            file_name = "file_" + str(count) + '.html'
+            file_name = "file_" + str(count)
             url = line
-            processURL(url, file_name, selector,process_only)
+            csv_object = processURL(url, file_name, selector, process_only)
+            csv_object_array.append(csv_object)
             count = count + 1
-
+  
+    saveCSV(csv_object_array)
+   
     print "Saved new file with " + str(limit) + " rows at: "
+
     f.close()
     return text
+
+
+def saveCSV(csv_object_array):
+    csv_file_name =  datetime.date.today().strftime("%Y-%B-%d")
+
+    csv_out = open("output/csv/" + csv_file_name + ".csv","w")
+    csv_out.seek(0)
+
+    csv = buildCSV(csv_object_array)
+
+    csv_out.write(csv)
+    
+    csv_out.truncate()
+    csv_out.close()
+
+def buildCSV(csv_object_array):
+    output = ''
+
+    
+
+    return output
+
+
 
 # Save sample as file.
 def save_sample(text, output_file_name):
@@ -48,31 +77,57 @@ def processURL(url, file_name, selector,process_only):
     r = requests.get(url)
     content = r.content
     
+    # header_r = requests.head(url=url, auth=auth)
+    # print r.headers
 
-    storeURLResults(content, file_name, selector,process_only)
+    results = storeURLResults(content, file_name, selector,process_only)
+    output = compressResultsCSV(file_name, results)
+    
+    return output
 
+def compressResultsCSV(file_name, results):
+    count = 0
+    lines = results.split('\n')
+    output = ''
+
+    csv_object = {}
+
+    for line in lines:
+      l = line.split(',')
+      
+      if count == 0:
+
+        csv_object['Dataset Name'] = l[0]
+        if len(l) > 1:
+          csv_object['Dataset Description'] = l[1]
+
+      else:
+          if len(l) > 1:
+              csv_object[l[0].strip(':')] = l[1] 
+      count = count + 1 
+
+    return csv_object
 
 def storeURLResults(text, file_name, selector, process_only):
     # Store results from visit to web page.
-    if process_only:
-      with open("output/processed/" + file_name, "wb") as write_file:
+    if process_only == True:
+      with open("output/processed/" + file_name + ".txt", "wb") as write_file:
           print "Processing data only."
-          with open("output/raw/" + file_name, 'r') as read_file:
-
-            
+          with open("output/raw/" + file_name + ".html", 'r') as read_file:
             text = read_file.read()
-            
             content = getFileDetails(text,selector)
-            content = content + limitResultsBySelector(text,selector) 
+            content = content + limitResultsBySelector(text,selector)
             write_file.write(str(content) + "\n")
+            return str(content)
     else:
-        with open("output/raw/" + file_name, "wb") as f:
+        with open("output/raw/" + file_name + '.html', "wb") as f:
             f.write(text)
 
-        with open("output/processed/" + file_name, "wb") as f:
+        with open("output/processed/" + file_name + ".txt", "wb") as f:
             content = getFileDetails(text,selector)
             content = content + limitResultsBySelector(text,selector) 
             f.write(str(content) + "\n")
+            return str(content)
 
 def getFileDetails(text,selector):
 
@@ -138,15 +193,19 @@ def main():
             else:
                 print "No selector given"  
                 selector = ''
-
+            
+            process = False
+            
             if len(sys.argv) > 4:
                 process_only = str(sys.argv[4])
-                if process_only == 'process_only':
-                  process_only = true
-            else:
-                process_only = false
+                print process_only
+                if process_only == 'process-only':
 
-            url_results = process_file(input_path, limit, selector, process_only)
+                  process = True
+
+
+
+            url_results = process_file(input_path, limit, selector, process)
 
         else:
             "File supplied not CSV"
